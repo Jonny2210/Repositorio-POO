90adc97c47ef2e70cf33f6d3095f8f3174ba77c6
Jonny2210 <125933400+Jonny2210@users.noreply.github.com>
2023-05-08 23:07:06 -0600

    Add files via upload

---
+++ Tarea 13 Introduccion a Java Swing/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Ventana ventana = new Ventana("Layouts");
+
+        //VentanaGrid venta = new VentanaGrid("Ventana Layout Cuadricula");
+
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Faranheid");
+
+
+
+    }
+}
